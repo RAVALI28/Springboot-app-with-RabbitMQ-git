d8028ea3ae0aedef1e4b6a8d15644d4790d24685
RAVALI28 <ravalikavajinapally.rv@gmail.com>
2025-04-17 11:30:54 -0500

    Initial commit

---
+++ src/main/java/com/DemoSpringbootAppWithRabbitMQ/Springboot/app/with/RabbitMQ/SpringbootAppWithRabbitMqApplication.java
@@ -0,0 +1,13 @@
+package com.DemoSpringbootAppWithRabbitMQ.Springboot.app.with.RabbitMQ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAppWithRabbitMqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAppWithRabbitMqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/DemoSpringbootAppWithRabbitMQ/Springboot/app/with/RabbitMQ/SpringbootAppWithRabbitMqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DemoSpringbootAppWithRabbitMQ.Springboot.app.with.RabbitMQ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAppWithRabbitMqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
